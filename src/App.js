d55cf7a3238d2ce90a50b5f7fa7cc91aacfcb343
gyuhyeong <hyounghe0724@naver.com>
2022-10-04 18:19:14 +0900

    7.2 this not version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+//component is just return jsx
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [having, setHaving] = useState("");
+  const [Btc, setBtc] = useState("");
+
+  function onChange(event) {
+    setHaving(event.target.value);
+  }
+  function handleChange(event) {
+    setBtc(event.target.value);
+  }
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={handleChange}>
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price} key={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <input
+        onChange={onChange}
+        value={having}
+        type="number"
+        placeholder="having your dollars"
+      ></input>
+      <h2>{having / Btc} BTC</h2>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1. props나 state가 업데이트
+// 2. 컴포넌트 리렌더링
+// 3. 이전 이펙트 클린업
+// 3-1. 컴포넌트가 파괴될 경우 새로운 이펙트가 없어지기때문에
+// 3-2. return 함수로 파괴될때의 콜백 함수를 설정(자유)
+// 4. 새로운 이펙트 실행
+// 출처: https://simsimjae.tistory.com/401 [104%:티스토리]
